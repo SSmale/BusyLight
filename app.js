d6f355f5137ff595306ac7da820c2ab6aec8702a
Simon Smale <simon@smalemail.net>
2020-03-22 18:24:16 +0000

    new changes

--- app.js
+++ app.js
@@ -9,66 +9,75 @@ ledController.init(NUMBER_OF_LEDS);
 ledController.setBrightness(255); // A value between 0 and 255
 
 // The RGB colours of the LEDs. for instance 0xff0000 is red, 0x0000ff is blue, etc.
-const leds = [
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0    
+const pixelData = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0
 ];
 
-ledController.render(leds);
+ledController.render(pixelData);
 
-function changeAll(r,g,b){
-    leds.map((x,i) => leds[i] = rgb2Int(r,g,b));
-    console.log(leds);
-    ledController.render(leds);
+function changeAll(r, g, b) {
+    pixelData.map((x, i) => pixelData[i] = rgb2Int(r, g, b));
+    ledController.render(pixelData);
 }
 
 app.get('/simon/dnd', (req, res) => {
-    changeAll(255,0,0);
-    res.send(200);
+    changeAll(255, 0, 0);
+    res.sendStatus(200);
 })
 
 app.get('/simon/busy', (req, res) => {
-    changeAll(255,211,0);
-    res.send(200);
+    changeAll(255, 211, 0);
+    res.sendStatus(200);
 })
 
 app.get('/simon/free', (req, res) => {
-    changeAll(0,255,0);
-    res.send(200);
+    changeAll(0, 255, 0);
+    res.sendStatus(200);
 })
 
-app.get('/kayleigh', () => {
+app.get('/party', (req, res) => {
+    const old = [...pixelData];
+    let offset = 0;
+    const intervalId = setInterval(function () {
+        for (var i = 0; i < NUMBER_OF_LEDS; i++) {
+            pixelData[i] = colorwheel((offset + i) % 256);
+        }
 
+        offset = (offset + 1) % 256;
+        ledController.render(pixelData);
+    }, 1000 / 30);
+
+    setTimeout(() => {
+        clearInterval(intervalId);
+        ledController.render(old)
+    },5000)
+
+    res.sendStatus(200);
+})
+
+app.get('/kayleigh', (req, res) => {
+    res.sendStatus(200)
+})
+
+app.get('/reset', (req, res) => {
+    changeAll(0, 0, 0);
+    res.sendStatus(200)
 })
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
-  });
-  
+});
+
 function colorwheel(pos) {
-pos = 255 - pos;
-if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
-else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
-else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
+    pos = 255 - pos;
+    if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
+    else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
+    else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
 }
 
 function rgb2Int(r, g, b) {
-return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+    return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
 }
-// // Loop every 0.1 sec: randomise an index and a colour and display it
-// const interval = setInterval(function() {
-//     const randomIndex = Math.floor(Math.random() * NUMBER_OF_LEDS);
-//     const randomColour = Math.floor(Math.random() * 0xffffff);
-//     leds[randomIndex] = randomColour;
-//     console.log('Rendering colour ' + randomColour + ' at index ' + randomIndex);
-//     ledController.render(leds);
-// }, 100);
-
-// // After 10 secondes, stop.
-// setTimeout(function () {
-//     console.log('Stop!');
-//     clearInterval(interval);
-//     ledController.reset();
-// }, 10000);
